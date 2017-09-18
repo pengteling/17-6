d845f3d4e5ab142fd867cf360ee1f6b865568e69
pengteling <pengteling@gmail.com>
2017-09-18 15:05:02 +0800

    test

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+
+var gulp = require('gulp');
+// npm i browser-sync
+/*实时监测 浏览器实时变化*/
+var browserSync = require('browser-sync');
+gulp.task('browser-sync', function() {
+    var files = [
+        'demo/**/*.html',
+        'demo/style/**/*.css',
+        'demo/js/**/*.js',
+        'demo/images/**/*.{png,jpg,jpeg,svg,gif,ico}'
+    ];
+    browserSync.init(files, {
+        server: {
+            baseDir: './demo/'
+        }
+    });
+});
+gulp.task('default',['browser-sync']);
