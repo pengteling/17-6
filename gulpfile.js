c26e8ce3e281cc3302b965ac2603bb428d7f8db3
pengteling <pengteling@gmail.com>
2017-12-29 22:08:49 +0800

    20171229

--- gulpfile.js
+++ gulpfile.js
@@ -17,4 +17,27 @@ gulp.task('browser-sync', function() {
         }
     });
 });
-gulp.task('default',['browser-sync']);
+//gulp.task('default',['browser-sync']);
+
+
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber'); //防止gulp退出进程
+gulp.task('less', function() { 
+    return gulp.src('./less/**/*.less')
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(less())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./less/css'))
+      // .pipe(browserSync.reload({
+      //            stream: true
+      //        }));
+  });
+
+gulp.task( 'watch', function() { // 开启监听
+  gulp.watch("less/**/*.less", ['less']);  // ./less路径 不监控 新增加的，如果去掉 ./ 则可以监控 less中新增加的less文件改动
+});
+
+gulp.task('default',['less','watch','browser-sync']);
+
